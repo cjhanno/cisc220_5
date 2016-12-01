1a03a49ea67e013c54325425d6da53eb548abcc6
Connor Hannough <12ch33@queensu.ca>
2016-12-01 15:55:39 -0800

    hello!

--- universalDoubleLinkeList.c
+++ universalDoubleLinkeList.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
+union Data {
+        int i;
+        int p*;
+        float f;
+        float q*;
+        char c;
+        char r*;
+};
+
+int main() {
+        union Data data;
+        printf( "Union Data size : %d\n", sizeof(union Data));
+
+        return 0; }
+
