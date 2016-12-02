fa5cae907242a406bda801c467dd9effbd7f1f20
Connor Hannough <12ch33@queensu.ca>
2016-12-01 16:06:37 -0800

    wow I hate this

--- universalDoubleLinkeList.c
+++ universalDoubleLinkeList.c
@@ -3,16 +3,26 @@
 
 union Data {
         int i;
-        int p*;
+        int p* = &i;
         float f;
-        float q*;
+        float q* = &f;
         char c;
-        char r*;
+        char r* = &c;
 };
 
 int main() {
         union Data data;
         printf( "Union Data size : %d\n", sizeof(union Data));
+
+        data.i = 13;
+        printf( "Data.i : %d\n", data.i);
+
+        data.f = 13.11;
+        printf( "Data.f : %f\n", data.f);
+
+        data.c = "a";
+        printf( "Data.c : %c\n", data.c);
+
 
         return 0; }
 
